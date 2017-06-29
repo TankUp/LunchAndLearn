d86ff17b1a13b0cc9623f0a296d9f8462bec8e37
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-29 13:19:36 +0300

    Fixed model relationships

--- models.rb
+++ models.rb
@@ -1,12 +1,16 @@
 class Video < ActiveRecord::Base
+  has_many :event_videos
   has_many :events, :through => :event_videos
 end
 
 class Person < ActiveRecord::Base
+  has_many :event_participants
   has_many :events, :through => :event_participants
 end
 
 class Event < ActiveRecord::Base
+  has_many :event_participants
+  has_many :event_videos
   has_many :people, :through => :event_participants
   has_many :videos, :through => :event_videos
 end
