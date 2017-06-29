059dd489998a09a82ad1b9437565312f7f2876e5
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-29 12:46:31 +0300

    Created DB schema

---
+++ db_init.rb
@@ -0,0 +1,54 @@
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new(File.open('database.log', 'w'))
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3', database: 'database.db'
+)
+
+ActiveRecord::Schema.define do
+  unless ActiveRecord::Base.connection.tables.include? 'events'
+    create_table :events do |table|
+      table.column :week,     :integer
+      table.column :video_id, :integer
+      table.column :venue_id, :integer
+      table.column :manager_id, :integer, through: 'people'
+      table.column :event_datetime, :datetime
+      table.column :monday_votes, :integer
+      table.column :tuesday_votes, :integer
+      table.column :wednesday_votes, :integer
+      table.column :thursday_votes, :integer
+      table.column :friday_votes, :integer
+    end
+  end
+
+  unless ActiveRecord::Base.connection.tables.include? 'event_participants'
+    create_table :event_participants do |table|
+      table.column :event_id, :integer
+      table.column :person_id, :integer
+    end
+  end
+
+  unless ActiveRecord::Base.connection.tables.include? 'people'
+    create_table :people do |table|
+      table.column :slack_name, :string
+    end
+  end
+
+  unless ActiveRecord::Base.connection.tables.include? 'videos'
+    create_table :videos do |table|
+      table.column :url, :string
+      table.column :total_votes, :integer
+      table.column :views, :integer
+      table.column :like_ratio, :float
+    end
+  end
+
+  unless ActiveRecord::Base.connection.tables.include? 'event_videos'
+    create_table :event_videos do |table|
+      table.column :event_id, :integer
+      table.column :video_id, :integer
+      table.column :votes, :integer
+    end
+  end
+end
