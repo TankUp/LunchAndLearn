338adfb3ad157556d7ba0bf4006358ab80469211
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-29 18:22:45 +0300

    Moved main_channele_name comment up top and updated gemfile.lock

--- main.rb
+++ main.rb
@@ -7,6 +7,8 @@ Slack.configure do |config|
   ENV['SLACK_API_TOKEN'] = $slack_token
   config.token = ENV['SLACK_API_TOKEN']
 end
+# Read the main channel where the poll will be held
+# main_channel_name = gets.chomp
 main_channel_name = 'nobodies'
 client = Slack::Web::Client.new
 channels = client.channels_list.channels
@@ -14,11 +16,5 @@ $main_channel = channels.detect { |c| c.name == main_channel_name }.id
 
 # run the slackbot
 require './lunch_bot/lib/lunch_bot'
-
-# Read the main channel where the poll will be held
-# main_channel_name = gets.chomp
-
-
-# client.chat_postMessage(channel: '#core_team_2', text: 'Hello World', as_user: true)
 
 LunchAndLearnBot.run
