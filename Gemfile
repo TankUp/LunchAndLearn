5bc5d0dfeb13e2522852b4b8e0c59bf918852ad7
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-29 16:10:25 +0300

    Merge gemfiles and updated main.rb

--- Gemfile
+++ Gemfile
@@ -2,3 +2,13 @@ source 'https://rubygems.org'
 
 gem 'activerecord'
 gem 'sqlite3'
+
+gem 'slack-ruby-bot'
+gem 'celluloid-io'
+gem 'eventmachine'
+gem 'faye-websocket'
+gem 'json', '~> 1.8', '>= 1.8.6'
+
+group :dev do
+  gem 'pry'
+end
--- main.rb
+++ main.rb
@@ -1,13 +1,7 @@
 require './db_init'
 require './models'
 
-ilya =  Person.new(slack_name: 'Ilya')
-stan =  Person.new(slack_name: 'Stanislav')
-plmn =  Person.new(slack_name: 'Plamen')
+# run the slackbot
+require './lunch_bot/lib/lunch_bot'
 
-vid = Video.new(url: 'tank')
-table_tennis_room = Venue.new(name: 'TABLE TENNIS :)')
-
-ev = Event.new(week: 1)
-ev.save!
-ev.people << stan
+LunchAndLearnBot.run
