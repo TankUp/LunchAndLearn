1bc28a9aa9965ff2d5f0316e10c8a5eb6ad7d6c8
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-29 15:42:25 +0300

    Added the ruby bot gem

--- lunch_bot/lib/lunch_bot.rb
+++ lunch_bot/lib/lunch_bot.rb
@@ -1,12 +1,17 @@
-require 'slack-ruby-client'
+require 'slack-ruby-bot'
 
 
 
 Slack.configure do |config|
-  print ENV['SLACK_API_TOKEN']
+  ENV['SLACK_API_TOKEN'] = 'xoxb-205565088373-CbkZFRI2qTT9KXvU0rQ8l2Ym'
   config.token = ENV['SLACK_API_TOKEN']
 end
+# Run command is
+# SLACK_API_TOKEN=xoxb-205565088373-CbkZFRI2qTT9KXvU0rQ8l2Ym bundle exec ruby lunch_bot.rb
+class Weather < SlackRubyBot::Bot
+  match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
+    client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
+  end
+end
 
-client = Slack::Web::Client.new
-
-client.auth_test
+Weather.run
