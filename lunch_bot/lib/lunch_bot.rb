f6c27da4dab7c25021b4a3cfbb0a278fe5a59cb4
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-29 16:07:09 +0300

    Bot now recognizes when we send it a YT link in DM

--- lunch_bot/lib/lunch_bot.rb
+++ lunch_bot/lib/lunch_bot.rb
@@ -1,17 +1,21 @@
 require 'slack-ruby-bot'
 
-
-
 Slack.configure do |config|
   ENV['SLACK_API_TOKEN'] = 'xoxb-205565088373-CbkZFRI2qTT9KXvU0rQ8l2Ym'
   config.token = ENV['SLACK_API_TOKEN']
 end
 # Run command is
 # SLACK_API_TOKEN=xoxb-205565088373-CbkZFRI2qTT9KXvU0rQ8l2Ym bundle exec ruby lunch_bot.rb
-class Weather < SlackRubyBot::Bot
-  match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
-    client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
+
+class LunchAndLearnBot < SlackRubyBot::Bot
+  match /(?<link>http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?)/ do |client, data, match|
+    youtube_link = match[:link]
+    group_is_dm = data.channel[0] == 'D'
+    if group_is_dm
+      client.say(channel: data.channel, text: "Thanks for the video, I will add it as a suggestion for the next event :)")
+    end
   end
+
 end
 
-Weather.run
+LunchAndLearnBot.run
