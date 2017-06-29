ba9f921f3f11ca0c51cb723c78a5cecb5fe952ad
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-29 13:34:28 +0300

    Added EventVideo model and pick_final_video() method

--- models.rb
+++ models.rb
@@ -13,6 +13,20 @@ class Event < ActiveRecord::Base
   has_many :event_videos
   has_many :people, :through => :event_participants
   has_many :videos, :through => :event_videos
+
+  # Picks the Video that is going to be watched at the Event
+  def pick_final_video
+    event_videos.all.max.video
+  end
+end
+
+class EventVideo < ActiveRecord::Base
+  belongs_to :event
+  belongs_to :video
+
+  def <=>(other_vid)
+    self.votes <=> other_vid.votes
+  end
 end
 
 class EventParticipant < ActiveRecord::Base
