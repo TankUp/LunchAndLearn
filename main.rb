e5a43f2dbf6d2406e062135616fdcf3240fcf409
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-06-30 15:09:57 +0300

    Extended DateTime and Time class functionality

--- main.rb
+++ main.rb
@@ -3,6 +3,45 @@ require './lib/models'
 require './slack_token'
 require './lib/commands_help.rb'
 require 'slack-ruby-bot'
+require 'date'
+class DateTime
+  def next_week
+    self + (7 - self.wday)
+  end
+
+  def next_wday (n)
+    if n.is_a?(String) || n.is_a?(Symbol)
+      puts 'ash'
+      n = case n
+            when /[Mm]onday/
+              1
+            when /[Tt]uesday/
+              2
+            when /[Ww]ednesday/
+              3
+            when /[Tt]hursday/
+              4
+            when /[Ff]riday/
+              5
+            else
+              0
+          end
+    end
+    n > self.wday ? self + (n - self.wday) : self.next_week.next_day(n)
+  end
+end
+
+class Time
+  def get_hour_difference_from(other_time)
+    raise Exception.new('Expected a Time object!') unless other_time.is_a?(Time)
+    if other_time > self
+      ((self - other_time) / 3600).round
+    else
+      ((other_time - self) / 3600).round
+    end
+  end
+end
+
 
 Slack.configure do |config|
   ENV['SLACK_API_TOKEN'] = $slack_token
